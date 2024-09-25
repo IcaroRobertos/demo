c56b778c945855d011aac80a11ac1e5a1a8f22a2
Icaro Roberto <icarosantos@ciandt.com>
2024-09-25 11:49:34 -0300

    demo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.json({branch: "main"})
+    res.json({branch: "develop"})
 })
 
 app.listen(8000, () => console.log("Running on port 8000"))
